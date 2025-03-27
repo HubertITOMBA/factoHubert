f0c877940858c9bad320d1289f5fbb2a7bcbd1ad
HubertITOMBA <hubert.itomba@gmail.com>
2025-03-27 17:02:08 +0100

    update ternary

--- actions/action.ts
+++ actions/action.ts
@@ -105,7 +105,7 @@ export async function createInvoice(prevState: any, formData: FormData) {
         invoiceLink:
           process.env.NODE_ENV !== "production"
             ? `http://localhost:3000/api/invoice/${data.id}`
-            : `https://invoice-marshal.vercel.app/api/invoice/${data.id}`,
+            : `https://facto-hubert-78dz.vercel.app/api/invoice/${data.id}`,
       },
     });
   
@@ -172,7 +172,7 @@ export async function createInvoice(prevState: any, formData: FormData) {
         invoiceLink:
           process.env.NODE_ENV !== "production"
             ? `http://localhost:3000/api/invoice/${data.id}`
-            : `https://invoice-marshal.vercel.app/api/invoice/${data.id}`,
+            : `https://facto-hubert-78dz.vercel.app/api/invoice/${data.id}`,
       },
     });
   
